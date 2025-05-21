27934ff84da50a035f731acc8e45c22da176d480
dammarin <marinatosd@gmail.com>
2025-05-21 20:01:38 +0300

    feat: using states in components

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,32 @@
-import ClassComponent from "./components/ClassComponent.tsx";
-import FunctionalComponent from "./components/FunctionalComponent.tsx";
-import ArrowFunctionalComponent from "./components/ArrowFunctionalComponent.tsx";
-import ArrowFunctionalComponentWithProps from "./components/ArrowFunctionalComponentWithProps.tsx";
-import ArrowFunctionalComponentWithPropsType from "./components/ArrowFunctionalComponentWithPropsType.tsx";
+// import ClassComponent from "./components/ClassComponent.tsx";
+// import FunctionalComponent from "./components/FunctionalComponent.tsx";
+// import ArrowFunctionalComponent from "./components/ArrowFunctionalComponent.tsx";
+// import ArrowFunctionalComponentWithProps from "./components/ArrowFunctionalComponentWithProps.tsx";
+// import ArrowFunctionalComponentWithPropsType from "./components/ArrowFunctionalComponentWithPropsType.tsx";
+import Layout from "./components/Layout.tsx";
+// import ClassComponentWithState from "./components/ClassComponentWithState.tsx";
+import FunctionalComponentWithState from "./components/FunctionalComponentWithState.tsx";
+
 
 function App() {
 
   return (
     <>
-      <ClassComponent/>
-      <FunctionalComponent/>
-      <ArrowFunctionalComponent/>
-      <ArrowFunctionalComponentWithProps title="Is a Arrow Functional Component with Props!"/>
-      <ArrowFunctionalComponentWithPropsType
-        title="Is a Arrow Functional Component with Props!"
-        description="this is a description"
-      />
+      <Layout>
+      {/*<ClassComponent/>*/}
+      {/*<FunctionalComponent/>*/}
+      {/*<ArrowFunctionalComponent/>*/}
+      {/*<ArrowFunctionalComponentWithProps title="Is a Arrow Functional Component with Props!"/>*/}
+      {/*<ArrowFunctionalComponentWithPropsType*/}
+      {/*  title="Is a Arrow Functional Component with Props!"*/}
+      {/*  description="this is a description"*/}
+      {/*/>*/}
+
+        {/*<ClassComponentWithState/>*/}
+        <FunctionalComponentWithState/>
+
+
+      </Layout>
     </>
   )
 }
