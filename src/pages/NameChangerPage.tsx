3389c28a64d6962b1c34cfe1b75fa4cac80141b2
dammarin <marinatosd@gmail.com>
2025-06-04 19:32:50 +0300

    feat: add document titles

--- src/pages/NameChangerPage.tsx
+++ src/pages/NameChangerPage.tsx
@@ -1,6 +1,12 @@
+import {useEffect} from 'react';
 import NameChanger from "../components/NameChanger.tsx";
 
 const NameChangerPage = () => {
+
+  useEffect(()=>{
+    document.title = "CF7 Name Changer"
+  }, [])
+
   return (
     <>
       <NameChanger/>
