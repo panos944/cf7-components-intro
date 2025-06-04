edaf6ae153b95da11c244a65b0d07badaa69757e
dammarin <marinatosd@gmail.com>
2025-06-04 19:34:02 +0300

    feat: testing dynamic segments

--- src/App.tsx
+++ src/App.tsx
@@ -19,6 +19,9 @@ import Layout from "./components/Layout.tsx";
 import {BrowserRouter, Routes, Route} from "react-router";
 import HomePage from "./pages/HomePage.tsx";
 import NameChangerPage from "./pages/NameChangerPage.tsx";
+// import NameChanger from "./components/NameChanger.tsx";
+import OnlineStatusPage from "./pages/OnlineStatusPage.tsx";
+import UserPage from "./pages/UserPage.tsx";
 
 function App() {
 
@@ -58,8 +61,13 @@ function App() {
       <BrowserRouter>
         <Layout>
           <Routes>
-            <Route path="/" element={<HomePage />}/>
-            <Route path="name-changer" element={<NameChangerPage/>}/>
+            {/*<Route path="/" element={<HomePage />}/>*/}
+            <Route index element={<HomePage />}/>
+            <Route path="examples?">
+              <Route path="name-changer" element={<NameChangerPage/>}/>
+              <Route path="online-status" element={<OnlineStatusPage/>}/>
+            </Route>
+            <Route path="users/:userId" element={<UserPage />}/>
           </Routes>
         </Layout>
       </BrowserRouter>
