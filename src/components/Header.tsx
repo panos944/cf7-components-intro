403f1f4b93a176d3fbafefc72dba0544db12ff25
dammarin <marinatosd@gmail.com>
2025-06-04 19:32:05 +0300

    feat: use react-router Link instead a tag

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,4 @@
+import {Link} from "react-router";
 import CodingFactoryLogo from "./CodingFactoryLogo.tsx";
 
 const Header = () => {
@@ -6,7 +7,11 @@ const Header = () => {
       <header className="bg-[#782024] fixed w-full">
         <div className="container mx-auto px-4 flex items-center justify-between">
           <CodingFactoryLogo />
-          <a className="text-white hover:underline hover:underline-offset-4" href="/">Home</a>
+          <nav className="flex gap-4">
+            {/*<a href="/" className="text-white hover:underline hover:underline-offset-4">Home</a>*/}
+            <Link to="/" className="text-white hover:underline hover:underline-offset-4">Home</Link>
+            <Link to="/examples/name-changer" className="text-white hover:underline hover:underline-offset-4">Name Changer</Link>
+          </nav>
         </div>
       </header>
     </>
