0443fab699de71b172f6188a83939c0293f3fbe9
dammarin <marinatosd@gmail.com>
2025-06-17 20:12:07 +0300

    feat: add new product listing page
    feat: add sonner

--- src/App.tsx
+++ src/App.tsx
@@ -18,23 +18,24 @@
 // import OnlineStatus from "./components/OnlineStatus.tsx";
 import {BrowserRouter, Routes, Route} from "react-router";
 // import HomePage from "./pages/HomePage.tsx";
-import NameChangerPage from "./pages/NameChangerPage.tsx";
+// import NameChangerPage from "./pages/NameChangerPage.tsx";
 // import NameChanger from "./components/NameChanger.tsx";
-import OnlineStatusPage from "./pages/OnlineStatusPage.tsx";
-import UserPage from "./pages/UserPage.tsx";
+// import OnlineStatusPage from "./pages/OnlineStatusPage.tsx";
+// import UserPage from "./pages/UserPage.tsx";
 import RouterLayout from "./components/RouterLayout.tsx";
 // import ExamplesPage from "./pages/ExamplesPage.tsx";
-import RouterExamplesLayout from "./components/RouterExamplesLayout";
-import AutoRedirectPage from "./pages/AutoRedirectPage.tsx";
-import NotFoundPage from "./pages/NotFoundPage.tsx";
-import FocusInput from "./components/FocusInput.tsx";
-import UncontrolledInput from "./components/UncontrolledInput.tsx";
-import ControlledInput from "./components/ControlledInput.tsx";
+// import RouterExamplesLayout from "./components/RouterExamplesLayout";
+// import AutoRedirectPage from "./pages/AutoRedirectPage.tsx";
+// import NotFoundPage from "./pages/NotFoundPage.tsx";
+// import FocusInput from "./components/FocusInput.tsx";
+// import UncontrolledInput from "./components/UncontrolledInput.tsx";
+// import ControlledInput from "./components/ControlledInput.tsx";
 // import MultiFieldForm from "./components/MultiFieldForm.tsx";
-import ExamplesPage from "./pages/ExamplesPage.tsx";
+// import ExamplesPage from "./pages/ExamplesPage.tsx";
 // import MultiFieldFormWithValidation from "./components/MultiFieldFormWithValidation.tsx";
 // import MultiFieldFormWithZodValidation from "./components/MultiFieldFormWithZodValidation.tsx";
-import MultiFieldFormWithReactHook from "./components/MultiFieldFormWithReactHook.tsx";
+// import MultiFieldFormWithReactHook from "./components/MultiFieldFormWithReactHook.tsx";
+import ProductList from "@/pages/ProductList.tsx";
 
 function App() {
 
@@ -72,36 +73,40 @@ function App() {
       {/*</Layout>*/}
 
 
-
-
       <BrowserRouter>
           <Routes>
-            <Route element={<RouterLayout />}>
+            {/*<Route element={<RouterLayout />}>*/}
               {/*<Route path="/" element={<HomePage />}/>*/}
               {/*<Route index element={<HomePage />}/>*/}
               {/*<Route index element={<MultiFieldForm/>}/>*/}
               {/*<Route index element={<MultiFieldFormWithValidation/>}/>*/}
               {/*<Route index element={<MultiFieldFormWithZodValidation/>}/>*/}
-              <Route index element={<MultiFieldFormWithReactHook/>}/>
-              <Route path="users/:userId" element={<UserPage />}/>
-              <Route path="users" element={<UserPage />}/>
-            </Route>
+            {/*  <Route index element={<MultiFieldFormWithReactHook/>}/>*/}
+            {/*  <Route path="users/:userId" element={<UserPage />}/>*/}
+            {/*  <Route path="users" element={<UserPage />}/>*/}
+            {/*</Route>*/}
 
             {/*<Route path="examples?" >*/}
-            <Route path="examples"  element={<RouterExamplesLayout/>}>
-              <Route index element={<ExamplesPage/>}/>
-              <Route path="name-changer" element={<NameChangerPage/>}/>
-              <Route path="online-status" element={<OnlineStatusPage/>}/>
-              <Route path="auto-redirect" element={<AutoRedirectPage/>}/>
-              <Route path="controlled-input" element={<ControlledInput />}/>
-              <Route path="uncontrolled-input" element={<UncontrolledInput />}/>
-              <Route path="focus-input" element={<FocusInput />}/>
-            </Route>
+            {/*<Route path="examples"  element={<RouterExamplesLayout/>}>*/}
+            {/*  <Route index element={<ExamplesPage/>}/>*/}
+            {/*  <Route path="name-changer" element={<NameChangerPage/>}/>*/}
+            {/*  <Route path="online-status" element={<OnlineStatusPage/>}/>*/}
+            {/*  <Route path="auto-redirect" element={<AutoRedirectPage/>}/>*/}
+            {/*  <Route path="controlled-input" element={<ControlledInput />}/>*/}
+            {/*  <Route path="uncontrolled-input" element={<UncontrolledInput />}/>*/}
+            {/*  <Route path="focus-input" element={<FocusInput />}/>*/}
+            {/*</Route>*/}
 
-            <Route path="users/:userId" element={<UserPage />}/>
-            <Route path="users" element={<UserPage />}/>
+            {/*<Route path="users/:userId" element={<UserPage />}/>*/}
+            {/*<Route path="users" element={<UserPage />}/>*/}
             {/*<Route path="files/*" element={<FilePage/>}/>*/}
-            <Route path="*"  element={<NotFoundPage/>}/>
+            {/*<Route path="*"  element={<NotFoundPage/>}/>*/}
+
+            <Route path="products" element={<RouterLayout />}>
+              <Route index element={<ProductList />}/>
+              {/*<Route path="new" element={<Product/>}/>*/}
+              {/*<Route path=":productId" element={<Product/>}/>*/}
+            </Route>
 
           </Routes>
       </BrowserRouter>
